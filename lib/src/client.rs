7d7de587293ddbfa44d0c762db34ce20d4dbb0a3
xyz600 <xyz600600@gmail.com>
2021-07-10 00:20:02 +0900

    first http clientgit add src/ Cargo.toml

---
+++ lib/src/client.rs
@@ -0,0 +1,35 @@
+use async_std::task;
+use http_client::http_types::{Method, Request};
+use http_client::HttpClient;
+
+const API_TOKEN: &str = "09d3f025-0404-45de-8361-b5db9c6bfff6";
+const URL: &str = "https://poses.live";
+
+use http_client::h1::H1Client as Client;
+
+pub fn hello() -> Result<String, String> {
+    task::block_on(async {
+        let client = Client::new();
+
+        let mut req = Request::new(Method::Get, format!("{}/api/hello", URL).as_str());
+        req.insert_header("Authorization", format!("Bearer {}", API_TOKEN));
+        let maybe_res = client.send(req).await;
+        if let Ok(res) = maybe_res {
+            let mut res = res;
+            let ret = res.body_string().await.unwrap();
+            Ok(ret)
+        } else {
+            Err("fail to say hello".to_string())
+        }
+    })
+}
+
+#[test]
+fn test_post() {
+    let s = hello();
+    if let Ok(body) = s {
+        assert_eq!(body, "{\"hello\":\"xyz600\"}");
+    } else {
+        panic!("fail to say hello.");
+    }
+}
