f7e1554277038310d18ca9b278741210917b87f9
xyz600 <xyz600600@gmail.com>
2021-07-09 22:59:38 +0900

    json (de)serialiszer

--- lib/src/data.rs
+++ lib/src/data.rs
@@ -1,7 +1,9 @@
+use serde_json::Value;
+
 #[derive(Clone, Copy)]
 pub struct Point {
-    y: f64,
-    x: f64,
+    pub y: f64,
+    pub x: f64,
 }
 
 impl Point {
@@ -48,24 +50,24 @@ fn edges_to_json(vertices: &Vec<(usize, usize)>) -> String {
 }
 
 pub struct Hole {
-    vertices_: Vec<Point>,
+    pub vertices: Vec<Point>,
 }
 
 impl Hole {
     pub fn new() -> Hole {
-        Hole { vertices_: vec![] }
+        Hole { vertices: vec![] }
     }
 
     pub fn push(&mut self, p: Point) {
-        self.vertices_.push(p);
+        self.vertices.push(p);
     }
 
-    pub fn vertices(&self) -> &Vec<Point> {
-        &self.vertices_
+    pub fn clear(&mut self) {
+        self.vertices.clear();
     }
 
     pub fn to_json(&self) -> String {
-        vertices_to_json(&self.vertices_)
+        vertices_to_json(&self.vertices)
     }
 }
 
@@ -78,33 +80,45 @@ fn test_hole_to_json() {
 }
 
 pub struct Figure {
-    vertices_: Vec<Point>,
-    edges_: Vec<(usize, usize)>,
+    vertices: Vec<Point>,
+    edges: Vec<(usize, usize)>,
+    neighbors: Vec<Vec<usize>>,
 }
 
 impl Figure {
     pub fn new() -> Figure {
         Figure {
-            vertices_: vec![],
-            edges_: vec![],
+            vertices: vec![],
+            edges: vec![],
+            neighbors: vec![],
         }
     }
 
     pub fn push(&mut self, p: Point) {
-        self.vertices_.push(p);
+        self.vertices.push(p);
     }
 
     pub fn connect(&mut self, v1: usize, v2: usize) {
-        self.edges_.push((v1, v2));
+        if self.neighbors.len() < v1.max(v2) + 1 {
+            self.neighbors.resize(v1.max(v2) + 1, vec![]);
+        }
+        self.edges.push((v1, v2));
+        self.neighbors[v1].push(v2);
+        self.neighbors[v2].push(v1);
+    }
+
+    pub fn clear(&mut self) {
+        self.vertices.clear();
+        self.edges.clear();
     }
 
     pub fn to_json(&self) -> String {
         let mut buffer = String::new();
         buffer += "{";
         buffer += "\"edges\": ";
-        buffer += edges_to_json(&self.edges_).as_str();
+        buffer += edges_to_json(&self.edges).as_str();
         buffer += ", \"vertices\": ";
-        buffer += vertices_to_json(&self.vertices_).as_str();
+        buffer += vertices_to_json(&self.vertices).as_str();
         buffer += "}";
         buffer
     }
@@ -145,6 +159,71 @@ impl Problem {
         buffer += "}";
         buffer
     }
+
+    pub fn clear(&mut self) {
+        self.hole.clear();
+        self.figure.clear();
+    }
+
+    pub fn from_json(json: &str) -> Problem {
+        let mut problem = Problem::new();
+        let v = serde_json::from_str::<Value>(json).unwrap();
+        for point in v["hole"].as_array().unwrap() {
+            let p = point.as_array().unwrap();
+            let x = p[0].as_f64().unwrap();
+            let y = p[1].as_f64().unwrap();
+            problem.hole.push(Point::new(x, y));
+        }
+        for point in v["figure"]["edges"].as_array().unwrap() {
+            let p = point.as_array().unwrap();
+            let v1 = p[0].as_u64().unwrap() as usize;
+            let v2 = p[1].as_u64().unwrap() as usize;
+            problem.figure.connect(v1, v2);
+        }
+        for point in v["figure"]["vertices"].as_array().unwrap() {
+            let p = point.as_array().unwrap();
+            let x = p[0].as_f64().unwrap();
+            let y = p[1].as_f64().unwrap();
+            problem.figure.vertices.push(Point::new(x, y));
+        }
+        problem.epsilon = v["epsilon"].as_f64().unwrap() / 1e6;
+        problem
+    }
+}
+
+#[test]
+fn test_problem_from_json() {
+    let json = "{
+        \"hole\": [
+        [55, 80], [65, 95], [95, 95], [35, 5], [5, 5],
+        [35, 50], [5, 95], [35, 95], [45, 80]
+        ],
+        \"figure\": {
+        \"edges\": [
+        [2, 5], [5, 4], [4, 1], [1, 0], [0, 8], [8, 3], [3, 7],
+        [7, 11], [11, 13], [13, 12], [12, 18], [18, 19], [19, 14],
+        [14, 15], [15, 17], [17, 16], [16, 10], [10, 6], [6, 2],
+        [8, 12], [7, 9], [9, 3], [8, 9], [9, 12], [13, 9], [9, 11],
+        [4, 8], [12, 14], [5, 10], [10, 15]
+        ],
+        \"vertices\": [
+        [20, 30], [20, 40], [30, 95], [40, 15], [40, 35], [40, 65],
+        [40, 95], [45, 5], [45, 25], [50, 15], [50, 70], [55, 5],
+        [55, 25], [60, 15], [60, 35], [60, 65], [60, 95], [70, 95],
+        [80, 30], [80, 40]
+        ]
+        },
+        \"epsilon\": 150000
+        }";
+    let problem = Problem::from_json(json);
+    assert_eq!(problem.hole.vertices.len(), 9);
+    assert_eq!(problem.hole.vertices[0].x, 55.0);
+    assert_eq!(problem.figure.vertices.len(), 20);
+    assert_eq!(problem.figure.vertices[0].x, 20.0);
+    assert_eq!(problem.figure.edges.len(), 30);
+    assert_eq!(problem.figure.edges[0].0, 2);
+    assert_eq!(problem.figure.neighbors[0].len(), 2);
+    assert_eq!(problem.epsilon, 0.15);
 }
 
 #[test]
@@ -159,24 +238,20 @@ fn test_problem_to_json() {
 }
 
 pub struct Pose {
-    vertices_: Vec<Point>,
+    vertices: Vec<Point>,
 }
 
 impl Pose {
     pub fn new() -> Hole {
-        Hole { vertices_: vec![] }
+        Hole { vertices: vec![] }
     }
 
     pub fn push(&mut self, p: Point) {
-        self.vertices_.push(p);
-    }
-
-    pub fn vertices(&self) -> &Vec<Point> {
-        &self.vertices_
+        self.vertices.push(p);
     }
 
     pub fn to_json(&self) -> String {
-        vertices_to_json(&self.vertices_)
+        vertices_to_json(&self.vertices)
     }
 }
 
